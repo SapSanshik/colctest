bd3dbdf2721878c6a2b97286edc0ad1d0abf2889
SapSanshik <mr.miko2000@mail.ru>
2022-08-24 12:19:16 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+# калькулятор v2	
+#
+from turtle import back
+from colorama import init
+from colorama import Fore, Back, Style
+
+# use Colorama to make Termcolor work on Windows too
+init()
+
+print( Fore.BLACK )
+print( Back.GREEN )
+
+what = input("Что делаем? (+, -): " )
+
+print( Back.CYAN )
+
+a = float( input("Введите первое число: "))
+b = float( input("Введите второе число: "))
+
+print( Back.MAGENTA )
+
+if what == "+":
+	c = a + b
+	print("Результат: " + str(c))
+elif what == "-":
+	c = a - b
+	print("Результат: " + str(c))
+else:
+	print("Выбрана неверная операция!")
+
+input()
